53642583362957e3b07dac69d4939ea71a00a8fb
dannab <107487996+bbororo@users.noreply.github.com>
2023-06-14 19:59:20 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 296 ms, Memory: 17936 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -6,7 +6,6 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int star = sc.nextInt();
-        String line = "";
 
         for(int i = 1 ; i <= star; i++){
             for(int j = 1; j <= i; j++){
