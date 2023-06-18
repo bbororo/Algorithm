a80f9aa0afe099f30e3ed8a19d0b21dcaca09936
dannab <107487996+bbororo@users.noreply.github.com>
2023-06-18 18:54:02 +0900

    [Bronze V] Title: 개수 세기, Time: 220 ms, Memory: 17800 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,26 +1,26 @@
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
+        // n = 정수의 갯수
+        //  찾으려는 정수 v . 문제 ": 정수v가 몇개인지 찾아라
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] array = new int[N];
+
+        int num = sc.nextInt();
+        int[] array = new int[num];
         int cnt = 0;
 
-        for(int i = 0; i < N; i++) {
+        for(int i = 0; i < num; i++){
             array[i] = sc.nextInt();
         }
+        int v = sc.nextInt();
 
-        int v= sc.nextInt();
-
-        for(int i = 0; i < array.length; i++) {
-            if (v == array[i]){
+        for(int i = 0 ;i < num; i++){
+            if(array[i]== v){
                 cnt++;
             }
         }
-
         System.out.println(cnt);
-
-
     }
 }
