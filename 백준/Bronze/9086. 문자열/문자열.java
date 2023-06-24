989ff6273e6b215fad541caf3a320a2050f96304
dannab <107487996+bbororo@users.noreply.github.com>
2023-06-25 01:36:58 +0900

    [Bronze V] Title: 문자열, Time: 196 ms, Memory: 17472 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        for(int i = 0;i < num;i++){
+            String str = sc.next();
+            System.out.print(str.charAt(0));
+            System.out.println(str.charAt(str.length()-1));
+        }
+    }
+}
