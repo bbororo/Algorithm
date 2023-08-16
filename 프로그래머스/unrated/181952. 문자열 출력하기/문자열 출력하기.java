f413ccf3ecd808dd49923b9e933ac7954cd77443
dannab <107487996+bbororo@users.noreply.github.com>
2023-08-16 12:26:24 +0900

    [unrated] Title: 문자열 출력하기, Time: 187.55 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.java
+++ 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.java
@@ -1,11 +1,10 @@
-import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
 
-        String str = br.readLine();
-
-        System.out.println(str);
+        System.out.println(a);
     }
 }
