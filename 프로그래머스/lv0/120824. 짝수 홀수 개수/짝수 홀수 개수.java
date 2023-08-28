0007f1355bff40e7bd633d51e60e5aa9394ccdb8
dannab <107487996+bbororo@users.noreply.github.com>
2023-08-28 18:59:12 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -1,15 +1,10 @@
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int[2];
-        
-        for(int i= 0; i < num_list.length; i++) {
-            if(num_list[i] %2 ==0){
-                answer[0]++;
-            }
-            else {
-                answer[1]++;
-            }
+        int[] result = new int[2];
+        for(int i = 0; i<num_list.length; i++){
+            if(num_list[i]%2 == 0) result[0]++;
+            else result[1]++;
         }
-        return answer;
+        return result;
     }
 }
