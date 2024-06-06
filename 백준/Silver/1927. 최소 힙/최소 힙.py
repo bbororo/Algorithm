8a7dcd38eee4294aad11e1d0e45539d8d694f5a3
dannab <107487996+bbororo@users.noreply.github.com>
2024-06-06 13:30:34 +0900

    [Silver II] Title: 최소 힙, Time: 128 ms, Memory: 37044 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.py
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -4,7 +4,7 @@ from sys import stdin
 n = int(input())
 heap = []
 
-for i in range(n):
+for _ in range(n):
     a = int(stdin.readline())
     if a == 0:
         print(heapq.heappop(heap)) if heap else print(0)
